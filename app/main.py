74a90d70004293600ad6500a837b2655f9cc62a6
GOforce64 <109470706+GOforce64@users.noreply.github.com>
2026-05-06 17:14:53 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,13 +9,27 @@ def main():
         # Wait for user input
         command = input()
 
+        # exit command
         if command.lower() == "exit":
             mainLoop = False
             break
 
+        # echo command
         elif command.startswith("echo "):
             print(command[5:])
-            
+
+        # type command
+        elif command.startswith("type "):
+            if command[5:] == "echo":
+                print("echo is a shell builtin")
+            elif command[5:] == "exit":
+                print("exit is a shell builtin")
+            elif command[5:] == "type":
+                print("type is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
+
+        # unknown command
         else:
             print(f"{command}: command not found")
 
