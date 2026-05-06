48713827e27390150e6f8e775fbbb18a56d7ef5a
GOforce64 <109470706+GOforce64@users.noreply.github.com>
2026-05-06 17:40:23 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,28 @@
 import sys
+import os
+from pathlib import Path
 
+command_types = {"echo": "builtin", "exit": "builtin", "type": "builtin"}
+
+
+
+def find_executable(command):
+    found = False
+    path_value = os.environ.get("PATH", "")
+    if path_value:
+        for directory in path_value.split(os.pathsep):
+            if not directory:
+                continue
+            path = Path(directory)
+            if not path.is_dir():
+                continue
+            candidate = path / command # merges path object, example: candidate = Path("/usr/bin/python") (works OS independesnty to merge path strings)
+            if candidate.is_file() and os.access(candidate, os.X_OK):
+                print(f"{command} is {candidate}")
+                found = True
+                break
+    if not found:
+        print(f"{command}: not found")
 
 def main():
     mainLoop = True
@@ -9,29 +32,27 @@ def main():
         # Wait for user input
         command = input()
 
-        # exit command
-        if command.lower() == "exit":
-            mainLoop = False
-            break
+        # No input, new line
+        if command:
+            # exit command
+            if command.lower() == "exit":
+                mainLoop = False
+                break
 
-        # echo command
-        elif command.startswith("echo "):
-            print(command[5:])
+            # echo command
+            elif command.startswith("echo "):
+                print(command[5:])
 
-        # type command
-        elif command.startswith("type "):
-            if command[5:] == "echo":
-                print("echo is a shell builtin")
-            elif command[5:] == "exit":
-                print("exit is a shell builtin")
-            elif command[5:] == "type":
-                print("type is a shell builtin")
+            # type command
+            elif command.startswith("type "):
+                if command[5:] in command_types and command_types[cmd[5:]] == "builtin":
+                    print(f"{command[5:]} is a shell builtin")
+                else:
+                    find_executable(command[5:])
+
+            # unknown command
             else:
-                print(f"{command[5:]}: not found")
-
-        # unknown command
-        else:
-            print(f"{command}: command not found")
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
